a91bdb9aae40eaa78505f80314b114b7d5150744
Javier Pastor <jpastor@cerebelum.net>
2019-11-02 13:15:27 +0100

    add urbackup-server-2.4.11
    Move location sourcerfix.

---
+++ app-backup/urbackup-server/files/SourceFix/crear_parches_2.4.11.sh
@@ -0,0 +1,5 @@
+#Igual que 2.4.10
+diff -Naur urbackup-server-2.4.10/urbackupserver/server_update.cpp.orig urbackup-server-2.4.10/urbackupserver/server_update.cpp > urbackup-server-2.4.11-autoupdate-code.patch
+diff -Naur urbackup-server-2.4.10/urbackupserver/server_settings.cpp.orig urbackup-server-2.4.10/urbackupserver/server_settings.cpp > urbackup-server-2.4.11-autoupdate-config.patch
+diff -Naur urbackup-server-2.4.10/urbackupserver/www/js/templates.chash-f85173fea3869fb86c781b60569246a4.js.orig urbackup-server-2.4.10/urbackupserver/www/js/templates.chash-f85173fea3869fb86c781b60569246a4.js > urbackup-server-2.4.11-autoupdate-ui.patch
+diff -Naur urbackup-server-2.4.10/Makefile.in.orig urbackup-server-2.4.10/Makefile.in > urbackup-server-2.4.11-autoupdate-datafiles-gcc-fortify.patch
