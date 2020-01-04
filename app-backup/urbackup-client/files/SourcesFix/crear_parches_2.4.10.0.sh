e45f17fbbd3244c4ede308a3a6b141035837f855
Javier Pastor <jpastor@cerebelum.net>
2020-01-04 01:27:02 +0100

    Remove duplicate files

--- app-backup/urbackup-client/files/SourcesFix/crear_parches_2.4.10.0.sh
+++ app-backup/urbackup-client/files/SourcesFix/crear_parches_2.4.10.0.sh
@@ -1,2 +1,3 @@
-diff -Naur urbackup-client-2.4.10.0/Makefile.in.orig urbackup-client-2.4.10.0/Makefile.in > urbackup-client-2.4.10-Makefile.patch
-diff -Naur urbackup-client-2.4.10.0/defaults_client.orig urbackup-client-2.4.10.0/defaults_client > urbackup-client-2.4.10-conf.patch
+# Igual que crear_parches_2.4.9.0
+diff -Naur urbackup-client-2.4.9.0/Makefile.in.orig urbackup-client-2.4.9.0/Makefile.in > urbackup-client-2.4.10-Makefile.patch
+diff -Naur urbackup-client-2.4.9.0/defaults_client.orig urbackup-client-2.4.9.0/defaults_client > urbackup-client-2.4.10-conf.patch
