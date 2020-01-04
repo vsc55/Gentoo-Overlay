04ab6f4f07a14e18dad1ee141402ae591fc1d55a
Javier Pastor <jpastor@cerebelum.net>
2020-01-04 01:07:52 +0100

    Add urbackup-client-2.4.10

---
+++ app-backup/urbackup-client/files/SourcesFix/crear_parches_2.4.10.0.sh
@@ -0,0 +1,2 @@
+diff -Naur urbackup-client-2.4.10.0/Makefile.in.orig urbackup-client-2.4.10.0/Makefile.in > urbackup-client-2.4.10-Makefile.patch
+diff -Naur urbackup-client-2.4.10.0/defaults_client.orig urbackup-client-2.4.10.0/defaults_client > urbackup-client-2.4.10-conf.patch
